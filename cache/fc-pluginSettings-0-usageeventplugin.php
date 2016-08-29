beab67af3f7b0c98da308e3aa28b1ab63083fb29
Brian Coates <mrtnkaboom@aol.com>
2016-08-29 11:34:19 -0500

    initial ojs files after I have hidden some side bar items.

---
+++ cache/fc-pluginSettings-0-usageeventplugin.php
@@ -0,0 +1,3 @@
+<?php return array (
+  'uniqueSiteId' => '57bf05922e772',
+); ?>
---
+++ cache/fc-siteSettings-site.php
@@ -0,0 +1,23 @@
+<?php return array (
+  'contactEmail' => 
+  array (
+    'en_US' => 'cody.frieden@okstate.edu',
+  ),
+  'contactName' => 
+  array (
+    'en_US' => 'Open Journal Systems',
+  ),
+  'defaultMetricType' => '',
+  'oneStepReset' => false,
+  'preventManagerPluginManagement' => false,
+  'showDescription' => true,
+  'showThumbnail' => true,
+  'showTitle' => true,
+  'siteTheme' => '',
+  'title' => 
+  array (
+    'en_US' => 'Medical Journal',
+  ),
+  'useAlphalist' => false,
+  'usePaging' => false,
+); ?>
